92cde3c4bd313a4b144581ea16c6265eeee01971
aswin <aswinpdas83@gmail.com>
2022-09-12 17:00:29 +0530

    Expense filter Sort - SortBy date & SortBy amount

--- src/app.js
+++ src/app.js
@@ -12,12 +12,13 @@ import getVisibleExpense from './selectors/expenses'
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4000 }))
-store.dispatch(addExpense({ description: 'Gas bill' }))
-store.dispatch(setTextFilter('water'))
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
+// store.dispatch(setTextFilter('water'))
 
-setTimeout(() => {
-    store.dispatch(setTextFilter('bill'))
-}, 4000)
+// setTimeout(() => {
+//     store.dispatch(setTextFilter('bill'))
+// }, 4000)
 
 const state = store.getState();
 const visibleExpense = getVisibleExpense(state.expenses, state.filters);
