7e141bc3dce8974d5510e8d8ee01aa1e452a7da4
aswin <aswinpdas83@gmail.com>
2022-09-13 22:50:30 +0530

    state enclosed with constructor

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,14 +6,18 @@ import "react-dates/lib/css/_datepicker.css"
 
 
 export default class ExpenseForm extends React.Component {
-    state = {
-        description: "",
-        amount: "",
-        note: "",
-        createdAt: moment(),
-        calendarFocused: false,
-        error: ""
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: props.expense ? props.expense.description : "",
+            amount: props.expense ? props.expense.amount : "",
+            note: props.expense ? (props.expense.amount / 100).toString() : "",
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: ""
+        };
+    }
 
     onDesciptionChange = (e) => {
         const description = e.target.value;
@@ -51,7 +55,7 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => ({ error: "" }));
             this.props.onSubmit({
                 description: this.state.description,
-                amount: parseFloat(this.state.amount, 10),
+                amount: parseFloat(this.state.amount, 10) * 100,
                 createdAt: this.state.createdAt.valueOf(),
                 note: this.state.note
             })
