f70280d5fbf645e86aa69f7b954035cd8bc37963
aswin <aswinpdas83@gmail.com>
2022-09-14 14:11:02 +0530

    sample test cases with expect toBe

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,8 +1,20 @@
-const add = (a, b) => a + b + 1;
+const add = (a, b) => a + b;
+
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`
 
 test('Should add two numbers', () => {
     const result = add(5, 2);
-    if (result !== 7) {
-        throw new Error(`the value aded 5,2 and result is ${result}, expected result is 7.`)
-    }
-})
+    expect(result).toBe(7)
+});
+
+test('generate greeting from name.', () => {
+    const result = generateGreeting('Aswin');
+    expect(result).toBe('Hello Aswin!')
+});
+
+test('generate greeting for no name.', () => {
+    const result = generateGreeting();
+    expect(result).toBe('Hello Anonymous!')
+});
+
+
