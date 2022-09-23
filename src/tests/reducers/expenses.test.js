815c6b48fe60779ce22acccaacac92ab235681cc
aswin <aswinpdas83@gmail.com>
2022-09-23 23:10:14 +0530

    test case : reducers/expenses.js

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,45 @@
+import expensesReducer from "../../reducers/expenses";
+import expenses from '../fixtures/expenses';
+
+test('Should set default state', () => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual([]);
+});
+
+test('Should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('Should not remove expenses if id not found', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '12156'
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
+
+test('Should add an expense', () => {
+    const expense = {
+        id: '5',
+        description: 'travel',
+        note: '',
+        createdAt: 20000,
+        amount: 2500
+    }
+    const action = {
+        type: 'ADD_EXPENSE',
+        expense
+    };
+    const state = expensesReducer(expenses, action)
+    expect(state).toEqual([...expenses, expense])
+})
+
+test('Should edit an expense', () => {
+
+})
